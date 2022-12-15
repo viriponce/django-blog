a75d439c64d5f86f83d14815c0feeeee03f33d2b
Viridiana Ponce Angulo <102187795+viriponce@users.noreply.github.com>
2022-12-14 16:48:21 -0800

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import environ
+
 
 env = environ.Env()
 # reading .env file
